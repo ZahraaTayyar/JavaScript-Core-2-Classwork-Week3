db1288a3b4ef3bfba2f1262d51f401c98afc0376
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-15 14:04:25 +0000

    Update exercise.js

--- 1-exercises/exercise.js
+++ 1-exercises/exercise.js
@@ -23,6 +23,22 @@ Write JavaScript below that console.logs:
 
 */
 
+// 1.
+const pElements = document.querySelectorAll("p");
+console.log(pElements);
+
+// 2.
+const firstDiv = document.querySelector("div")
+console.log(firstDiv);
+
+// 3.
+const jumbotronText = document.querySelector("#jumbotronText")
+console.log(jumbotronText); 
+
+// 4.
+const pElementInPrimaryContent = document.querySelectorAll(".primary-content p")
+console.log(pElementInPrimaryContent);
+
 
 /*
 Task 2
@@ -30,6 +46,11 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+
+const alertBtn = document.querySelector('#alertBtn');
+alertBtn.addEventListener("click", () => {
+  alert("Thanks for visiting Bikes for Refugees")
+})
 
 
 /*
@@ -40,6 +61,13 @@ When a user clicks the 'Change colour' button, the background colour of the page
 Hint: can you do this with a CSS class instead of adding styles to the element?
 */
 
+const changeBackgroundBtn = document.querySelector('#bgrChangeBtn');
+changeBackgroundBtn.addEventListener("click", () => {
+  const bodySelector = document.querySelector("body")
+  // console.log(bodySelector.style.backgroundColor)
+  bodySelector.style.backgroundColor = "red"
+});
+
 
 /*
 Task 4
@@ -47,6 +75,14 @@ Task 4
 
 When a user clicks the 'Larger links!' button, the text of all links on the page should increase.
 */
+
+const largerLinksBtn = document.querySelector("#largerLinksBtn");
+largerLinksBtn.addEventListener("click", () => {
+  const linksSelector = document.querySelectorAll("a");
+  for (let i = 0; i < linksSelector.length; i++) {
+    linksSelector[i].style.fontSize = "30px";
+  }
+})
 
 
 /*
@@ -56,3 +92,31 @@ Task 5
 When a user clicks the 'Add some text' button, the text in the input field should be appended to the page as a new paragraph below it.
 Hint: the new paragraph should be appended to the element with id 'addArticle'.
 */
+
+const addTextButton = document.querySelector('#addArticleBtn');
+
+addTextButton.addEventListener("click", () => {
+  const text = document.querySelector('#addArticleInput').value;
+
+  const pElement = document.createElement("p");
+  pElement.innerText = text;
+
+  const parent = document.querySelector("#addArticle");
+  parent.appendChild(pElement);
+})
+
+
+
+// thank you message after "donate now" is clicked:
+
+const donateBtn = document.querySelector('#donateBtn');
+
+donateBtn.addEventListener("click", () => {
+  const thankYouMessage = document.createElement("div");
+
+  thankYouMessage.innerText = "Thank you for donating!";
+  thankYouMessage.style.textAlign = "right";
+
+  const parent = document.querySelector(".site-header")
+  parent.appendChild(thankYouMessage);
+})
